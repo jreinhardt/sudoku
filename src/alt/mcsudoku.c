d408730e956beea9652cf624efccf960f8bec4c5
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-05-16 09:07:18 +0200

    added missing license header

--- src/alt/mcsudoku.c
+++ src/alt/mcsudoku.c
@@ -1,6 +1,21 @@
-//Sudoku Solver using the Metropolis Algorithm
-//author: Johannes at ist-dein-freund dot de
-//04-24-2007
+/*
+ * Copyright (C) 2007-2013 Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Sudoku Solver using the Metropolis Algorithm */
 
 #include <stdio.h>
 #include <limits.h>
